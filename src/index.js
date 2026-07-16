92b7b119d24568e9b22ce6a5609ea8d86b61ebc5
Adam <adaminferrera@gmail.com>
2026-07-15 22:36:06 -0500

    Delete src/index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    url.pathname = url.pathname.replace(/^\/mahjong-play/, '') || '/';
-    return env.ASSETS.fetch(url);
-  },
-};
